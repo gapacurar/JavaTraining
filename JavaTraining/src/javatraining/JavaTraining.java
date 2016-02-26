69b7517b8b106d540e9365c3c811e7ae4a3006ae
gheorgheaurelpacurar <gheorgheaurelpacurar@10.0.1.30>
2016-02-26 13:06:43 +0200

    Added description to package comment and also added a new println.

---
+++ JavaTraining/src/javatraining/JavaTraining.java
@@ -0,0 +1,26 @@
+/*
+ * This is a package created to keep all Java Trainings examples of code.
+ * We'll create a separate branch for each training session.
+*/
+
+package javatraining;
+
+/**
+ *
+ * @author gheorgheaurelpacurar
+ * @version 0.2
+ * 
+ */
+public class JavaTraining {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This is just a small example ");
+        System.out.println("after cloning an existing GitHub Repository and ");
+        System.out.println("create a new project from this existing one having the name Java Training :)");
+    }
+    
+}
