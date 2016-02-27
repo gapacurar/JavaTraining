c644382979632763ba827ddbc4a043e1e62e0e48
gheorgheaurelpacurar <gheorgheaurelpacurar@10.0.1.30>
2016-02-27 18:38:05 +0200

    Create the class.

--- JavaTraining/src/ro/sci/HelloWorldApp.java
+++ JavaTraining/src/ro/sci/HelloWorldApp.java
@@ -10,5 +10,9 @@ package ro.sci;
  * @author gheorgheaurelpacurar
  */
 public class HelloWorldApp {
-    
+    public static void main(String[] args){
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("Result is: "+a+b);
+    }
 }
