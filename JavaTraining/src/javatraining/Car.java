c9057b7004eb9a9025ca45686405116062a7e1fc
gheorgheaurelpacurar <gheorgheaurelpacurar@10.0.1.30>
2016-03-03 09:42:32 +0200

    Class Car to explain data hiding or encapsulation.

---
+++ JavaTraining/src/javatraining/Car.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright a-sti.ro
+ */
+package javatraining;
+
+/**
+ *
+ * @author gheorgheaurelpacurar
+ */
+public class Car {
+    /**
+     @enum enumeration of accepted car colors
+     */
+    public enum Color { WHITE, RED, YELLOW, GREEN, GRAY, BLACK };
+    
+    /**
+     Internal hidden fields / attributes
+     */
+    private Color color;
+    private String name;
+    private short speed;
+    /**
+     Constructors
+     */
+    public Car(){
+        name = "Default car name";
+        speed = 90;
+    }
+    public Car(String carName, short carSpeed, Color carColor){
+        name = carName;
+        speed = carSpeed;
+        color = carColor;
+    }
+    /*get-ers and set-ers*/
+    public String getName(){
+        return(name);
+    }
+    public Color getColor(){
+        return(color);
+    }
+    public short getSpeed(){
+        return(speed);
+    }
+    public void setName(String newName){
+        name = newName;
+    }
+    public void setColor(Color newColor){
+        color = newColor;
+    }
+    public void setSpeed(short newSpeed){
+        speed = newSpeed;
+    }
+    /**
+     The method increase actual speed of Car until final speed specified.
+     * Method tests if specified speed is higher than actual speed before to 
+     * increase it.
+     * @param newSpeed is new speed 
+     */
+    public void increaseSpeed(short newSpeed){
+        if(newSpeed > speed){
+            speed = newSpeed;
+        }
+        else if (newSpeed < speed){
+            decreaseSpeed(newSpeed);
+        }
+    }
+    /**
+     The method decrease actual speed of Car until final speed specified.
+     * Method tests if specified speed is higher than actual speed before to 
+     * increase it.
+     * @param newSpeed is new speed 
+     */
+    public void decreaseSpeed(short newSpeed){
+        if(newSpeed < speed){
+            speed = newSpeed;
+        }
+        else if (newSpeed < speed){
+            increaseSpeed(newSpeed);
+        }
+    }
+}
