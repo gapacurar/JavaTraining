713ab109340f16c10f65739e81eed7811fe04825
gheorgheaurelpacurar <gheorgheaurelpacurar@10.0.1.30>
2016-06-17 20:50:09 +0300

    Final version of Java Training

--- JavaTraining/src/ioxml/ParseXMLUsingDOM.java
+++ JavaTraining/src/ioxml/ParseXMLUsingDOM.java
@@ -5,12 +5,16 @@ package ioxml;
 
 
 import java.io.File;
+import java.io.IOException;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 /**
  *
  * @author gheorgheaurelpacurar
@@ -27,9 +31,12 @@ public class ParseXMLUsingDOM {
          Document doc = dBuilder.parse(inputFile);
          doc.getDocumentElement().normalize();
          System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-         NodeList nList = doc.getElementsByTagName("bookstore");
+         NodeList nList = doc.getElementsByTagName("book");
          System.out.println("----------------------------");
-         for (int temp = 0; temp < nList.getLength(); temp++) {
+         int length = nList.getLength();
+         System.out.println("XML number of elements = "+length);
+         for (int temp = 0; temp < length; temp++) 
+         {
             Node nNode = nList.item(temp);
             System.out.println("\nCurrent Element :" + nNode.getNodeName());
             if (nNode.getNodeType() == Node.ELEMENT_NODE) {
@@ -60,7 +67,7 @@ public class ParseXMLUsingDOM {
                   .getTextContent());
             }
          }
-      } catch (Exception e) {
+      } catch (ParserConfigurationException | SAXException | IOException | DOMException e) {
          e.printStackTrace();
       }
    }
