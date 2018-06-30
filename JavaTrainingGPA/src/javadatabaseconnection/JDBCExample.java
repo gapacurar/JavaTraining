978653a092f42c8a849d9e990f28e2ae7f2c34da
gheor <gheor@DESKTOP-3GJF3LN>
2018-06-30 08:06:51 +0300

    Java8 Signed-off-by:gheor <gheor@DESKTOP-3GJF3LN>

--- JavaTrainingGPA/src/javadatabaseconnection/JDBCExample.java
+++ JavaTrainingGPA/src/javadatabaseconnection/JDBCExample.java
@@ -31,8 +31,8 @@ public class JDBCExample {
             statement = connection.createStatement();
             //System.out.println("Statement is "+statement.toString());
             //statement.execute("CREATE TABLE FIRSTTABLE (ID INT PRIMARY KEY, NAME VARCHAR(12))");
-            //statement.execute("INSERT INTO APP.FIRSTTABLE VALUES (10,'TEN'),(20,'TWENTY'),(30,'THIRTY')");
-            resultSet = statement.executeQuery("SELECT ID, NAME FROM APP.FIRSTTABLE");
+            statement.execute("INSERT INTO FIRSTTABLE VALUES (10,'TEN'),(20,'TWENTY'),(30,'THIRTY')");
+            resultSet = statement.executeQuery("SELECT ID, NAME FROM FIRSTTABLE");
             //boolean resultSetHasRows = resultSet.next(); 
             if (null!=resultSet)
             {
